7baaa25bcf129e7deac6ef3e527ffee32823ea6c
yone0000 <ytytyt.0108@gamil.com>
2021-05-12 16:13:34 +0900

    room_usersテーブルの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,6 @@ class RoomsController < ApplicationController
       end
 
       def create
-        # binding.pry
         @room = Room.new(room_params)
         if @room.save
           redirect_to root_path
